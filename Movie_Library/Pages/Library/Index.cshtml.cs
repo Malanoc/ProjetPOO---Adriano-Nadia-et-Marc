00da2a32da7c25560fc0a77c161c28f2985bead4
Marc <tm.crz1@gmail.com>
2026-09-08 15:59:09 +0200

    Amélioration de l'interface bibliothèque

--- Movie_Library/Pages/Library/Index.cshtml.cs
+++ Movie_Library/Pages/Library/Index.cshtml.cs
@@ -1,10 +1,17 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibraryClass = Movie_Library.Classes.Library;
 
 namespace Movie_Library.Pages.Library
 {
     public class IndexModel : PageModel
     {
+        internal LibraryClass Library { get; private set; }
+
+        public IndexModel()
+        {
+            Library = new LibraryClass("Ma bibliothèque");
+        }
+
         public void OnGet()
         {
         }
